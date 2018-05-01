1896ceea65eb834d9c9f775bc57061f2537b5faf
hustic <sotgouli@gmail.com>
2018-05-01 15:16:28 +0300

    changed colors

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@
 
 var gulp = require('gulp');
 var imageResize = require('gulp-image-resize');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+//var sass = require('gulp-sass');
+//var uglify = require('gulp-uglify');
+//var rename = require('gulp-rename');
 var del = require('del');
 
 gulp.task('resize', function () {
@@ -26,18 +26,19 @@ gulp.task('del', ['resize'], function () {
 });
 
 // compile scss to css
+/*
 gulp.task('sass', function () {
     return gulp.src('./assets/sass/main.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({basename: 'main.min'}))
         .pipe(gulp.dest('./assets/css'));
 });
-
+*/
 // watch changes in scss files and run sass task
-gulp.task('sass:watch', function () {
-    gulp.watch('./assets/sass/**/*.scss', ['sass']);
-});
-
+//gulp.task('sass:watch', function () {
+ //   gulp.watch('./assets/sass/**/*.scss', ['sass']);
+//});
+/*
 // minify js
 gulp.task('minify-js', function () {
     return gulp.src('./assets/js/main.js')
@@ -51,3 +52,4 @@ gulp.task('default', ['del']);
 
 // scss compile task
 gulp.task('compile-sass', ['sass', 'minify-js']);
+*/
